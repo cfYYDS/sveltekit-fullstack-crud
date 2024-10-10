cfdce68dcf09de3817192810648881464643d6ea
Slack <1048001021@qq.com>
2024-10-10 17:02:34 +0800

    finish project

---
+++ src/lib/components/TodoItem.svelte
@@ -0,0 +1,39 @@
+<script>
+    export let index;
+    export let todo;
+    export let editTodo;
+    export let removeTodo;
+</script>
+
+<div class="todo">
+    <p>
+        {index + 1}. {todo}
+    </p>
+    <div class="actions">
+        <i class="fa-regular fa-pen-to-square"></i>
+        <i on:click={() => removeTodo(index)} class="fa-regular fa-trash-can"
+        ></i>
+    </div>
+</div>
+
+<style>
+    .todo {
+        border-left: 1px solid cyan;
+        padding: 8px 14px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+    .actions {
+        display: flex;
+        align-items: center;
+        gap: 14px;
+        font-size: 1.3rem;
+    }
+    .actions i {
+        cursor: pointer;
+    }
+    .actions i:hover {
+        color: coral;
+    }
+</style>
